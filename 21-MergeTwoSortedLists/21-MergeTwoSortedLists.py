0e4fd3adf5ecb7095e8968d85c5c700e33b7ff48
Laksa Fadhil Yansen <166758385+laksayansen@users.noreply.github.com>
2026-09-08 17:34:19 -0400

    File updated at 9/8/2026, 5:34:19 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,12 +1,10 @@
-# Last updated: 9/8/2026, 5:33:33 PM
+# Last updated: 9/8/2026, 5:34:19 PM
 1class Solution:
 2    def removeDuplicates(self, nums: List[int]) -> int:
 3        k = 1
 4        for i in range(1, len(nums)):
-5            if nums[i] == nums[k - 1]:
-6                nums[i] = nums[k]
-7            else:
-8                nums[k] = nums[i]
-9                k += 1
-10        return k
-11
+5            if nums[i] != nums[k - 1]:
+6                nums[k] = nums[i]
+7                k += 1
+8        return k
+9
