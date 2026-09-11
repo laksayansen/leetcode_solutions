7ce66e8108fe74aad80a93bb6a59bc92f5fbe10a
Laksa Fadhil Yansen <166758385+laksayansen@users.noreply.github.com>
2026-09-11 08:33:27 -0400

    File updated at 9/11/2026, 8:33:26 AM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,9 +1,9 @@
-# Last updated: 9/8/2026, 6:08:37 PM
+# Last updated: 9/11/2026, 8:33:26 AM
 1class Solution:
-2    def removeElement(self, nums: List[int], val: int) -> int:
-3        k = 0
-4        for i in range(len(nums)):
-5            if nums[i] != val:
-6                nums[k] = nums[i]
-7                k += 1
-8        return k
+2    def strStr(self, haystack: str, needle: str) -> int:
+3        n, m = len(haystack), len(needle)
+4        for i in range(n - m + 1):
+5            if haystack[i:i+m] == needle:
+6                return i
+7        return -1
+8
